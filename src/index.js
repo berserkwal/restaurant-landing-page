92b42d012b284c6020673c3fef110836e4ab0129
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-06-19 21:47:29 +0530

    commit to test the working of pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("hello from webpack");
+import "./style.css";
+
+createdDOMElements = (function () {
+	const main = document.createElement("main");
+	document.body.append(main);
+	return { main };
+})();
